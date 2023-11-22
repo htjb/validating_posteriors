6922294fb6db364032fb9da3d26d6ea3b0f4b52e
Harry Bevins <harrytjbevins@yahoo.co.uk>
2023-11-22 10:49:40 +0000

    updates

--- train_emulators.py
+++ train_emulators.py
@@ -1,17 +1,19 @@
 from globalemu.preprocess import process
 from globalemu.network import nn
 import numpy as np
+from globalemu.eval import evaluate
+from globalemu.plotter import signal_plot
 
-def gen_emulator(base_dir, AFB=True, resampling=True):
+
+def gen_emulator(base_dir, AFB=True, resampling=True, resume=False):
     z = np.arange(6, 55, 0.1)
-    process('full', z, base_dir=base_dir, 
+    if resume != True:
+    	process('full', z, base_dir=base_dir, 
             data_location='signal_data/', logs=[0, 2, 4, 5],
             AFB=AFB, resampling=resampling)
-    nn(batch_size=451, epochs=10000, base_dir=base_dir, 
+    
+    nn(batch_size=451, epochs=500, base_dir=base_dir, resume=resume, 
        layer_sizes=[32, 32, 32], input_shape=9, early_stop=True)
-
-    from globalemu.eval import evaluate
-    from globalemu.plotter import signal_plot
 
     predictor = evaluate(base_dir=base_dir, logs=[0, 2, 4, 5])
 
@@ -22,8 +24,8 @@ def gen_emulator(base_dir, AFB=True, resampling=True):
         loss_label='RMSE = {:.4f} [mK]')
 
 uber_dir = 'emulators/'
-gen_emulator(uber_dir + 'with_AFB_resampling/', AFB=True, resampling=True)
-gen_emulator(uber_dir + 'with_AFB_only/', AFB=True, resampling=False)
-gen_emulator(uber_dir + 'with_resampling_only/', AFB=False, resampling=True)
+#gen_emulator(uber_dir + 'with_AFB_resampling/', AFB=True, resampling=True, resume=True)
+#gen_emulator(uber_dir + 'with_AFB_only/', AFB=True, resampling=False)
+#gen_emulator(uber_dir + 'with_resampling_only/', AFB=False, resampling=True, resume=True)
 gen_emulator(uber_dir + 'no_AFB_no_resampling/', AFB=False, resampling=False)
 
