e43cb91df355666484a32e4f486b1267d9195942
htjb <harrytjbevins@yahoo.co.uk>
2023-11-28 15:23:40 +0000

    playing

--- polychord_call.py
+++ polychord_call.py
@@ -52,8 +52,8 @@ def likelihood(theta):
     logL = np.sum(-0.5*np.log(2*np.pi*noise**2) - 0.5*(dT_obs - dT)**2/noise**2)
     return logL, []
 
-FIXED_NOISE = False
-ARES = False
+FIXED_NOISE = True
+ARES = True
 nv = 25
 z, dT_obs = np.loadtxt('ares_fiducial_model_noise_%d.txt' % nv, unpack=True)
 
