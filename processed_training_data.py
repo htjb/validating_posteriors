26a150b39f45f8c4bbb2051b3978e7f1c290bf23
htjb <harrytjbevins@yahoo.co.uk>
2023-12-18 11:24:36 +0000

    figure showing the preprocessing in action

--- processed_training_data.py
+++ processed_training_data.py
@@ -1,6 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matplotlib import rc
+import matplotlib as mpl
+
+# figure formatting
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler('color',
+    ['ff7f00', '984ea3', '999999', '377eb8', 
+     '4daf4a','f781bf', 'a65628', 'e41a1c', 'dede00'])
+mpl.rcParams['text.usetex'] = True
+rc('font', family='serif')
+rc('font', serif='cm')
+rc('savefig', pad_inches=0.05)
+
 ares_emulator = 'emulators/with_AFB_resampling/'
 zeus_emulator = 'zeus_emulators/with_AFB_resampling/'
 ares_data = 'signal_data/'
@@ -24,27 +36,27 @@ ares_tl = np.loadtxt(ares_data + 'test_labels.txt')
 zeus_td = np.loadtxt(zeus_data + 'test_data.txt')
 zeus_tl = np.loadtxt(zeus_data + 'test_labels.txt')
 
-fig, axes = plt.subplots(2, 3, figsize=(12, 6),
+fig, axes = plt.subplots(2, 3, figsize=(7, 3.5),
                          sharey='col')
 ax = axes.flatten()
 
-[ax[0].plot(ares_z, s, alpha=1, c=plt.get_cmap('summer')(i/100)) for i, s in enumerate(ares_tl[:100])]
-[ax[3].plot(zeus_z, s, alpha=1, c=plt.get_cmap('summer')(i/100)) for i, s in enumerate(zeus_tl[:100])]
+[ax[0].plot(ares_z, s, alpha=1, lw=0.8) for i, s in enumerate(ares_tl[:100])]
+[ax[3].plot(zeus_z, s, alpha=1, lw=0.8) for i, s in enumerate(zeus_tl[:100])]
 
-[ax[1].plot(ares_z, s - ares_AFB, alpha=1, c=plt.get_cmap('summer')(i/100)) for i, s in enumerate(ares_tl[:100])]
-[ax[4].plot(zeus_z, s - zeus_AFB, alpha=1, c=plt.get_cmap('summer')(i/100)) for i, s in enumerate(zeus_tl[:100])]
+[ax[1].plot(ares_z, s - ares_AFB, alpha=1, lw=0.8) for i, s in enumerate(ares_tl[:100])]
+[ax[4].plot(zeus_z, s - zeus_AFB, alpha=1, lw=0.8) for i, s in enumerate(zeus_tl[:100])]
 
 ares_interp = [np.interp(ares_resample, ares_z, s-ares_AFB) for s in ares_tl[:100]]
 # plots the resamples signals with even spaced x ticks
 # even if the resampled points are not evenly spaced
 [ax[2].plot(np.arange(0, len(ares_resample), 1),
     ares_interp[i]/ares_std, 
-    alpha=1, c=plt.get_cmap('summer')(i/100) ) for i in range(len(ares_interp))]
+    alpha=1, lw=0.8) for i in range(len(ares_interp))]
 
 idx = []
 for i in range(len(ares_resample)):
     if np.any(np.isclose(ares_resample[i], 
-                         [10, 20, 29.96, 40, 50], 
+                         [10, 20, 29.96, 35, 50], 
                          rtol=2e-3, atol=2e-3)):
         idx.append(i)
 idx.append(len(ares_resample)-1)
@@ -57,7 +69,7 @@ ax[2].set_xticklabels([
 zeus_interp = [np.interp(zeus_resample, zeus_z, s-zeus_AFB) for s in zeus_tl[:100]]
 [ax[5].plot(np.arange(0, len(zeus_resample), 1),
     zeus_interp[i]/zeus_std, 
-    alpha=1, c=plt.get_cmap('summer')(i/100)) for i in range(len(zeus_interp))]
+    alpha=1, lw=0.8) for i in range(len(zeus_interp))]
 
 idx = []
 for i in range(len(zeus_resample)):
@@ -80,8 +92,8 @@ ax[4].set_xlabel(r'$z$')
 ax[5].set_xlabel(r'$z$')
 ax[1].set_ylabel(r'$ T_{21} - T_{AFB}$ [mK]')
 ax[4].set_ylabel(r'$ T_{21} - T_{AFB}$ [mK]')
-ax[5].set_ylabel(r'$( T_{21} - T_{AFB})/\sigma_{T_{21}}$ [mK]')
-ax[2].set_ylabel(r'$( T_{21} - T_{AFB})/\sigma_{T_{21}}$ [mK]')
+ax[5].set_ylabel(r'$\frac{T_{21} - T_{AFB}}{\sigma_{T_{21}}}$')
+ax[2].set_ylabel(r'$\frac{T_{21} - T_{AFB}}{\sigma_{T_{21}}}$')
 
 ax[5].set_xlim(0, len(zeus_resample)-1)
 ax[2].set_xlim(0, len(ares_resample)-1)
@@ -89,6 +101,7 @@ for i in range(len(axes)):
     for j in range(axes.shape[-1]):
         if j !=2:
             axes[i, j].set_xlim(6, 55)
+            axes[i, j].set_xticklabels(axes[i, j].get_xticklabels(), rotation=45)
         axes[i, j].grid()
 
 plt.tight_layout()
