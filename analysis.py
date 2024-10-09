3060dff2a3bfaa09dee9e8b8c931c6526228ae71
htjb <harrytjbevins@yahoo.co.uk>
2024-10-09 11:59:20 +0100

    trying to plot dkls in a nice way

--- analysis.py
+++ analysis.py
@@ -9,11 +9,14 @@ ground_truth[5] = np.log10(ground_truth[5])
 ground_truth[4] = np.log10(ground_truth[4])
 ground_truth[2] = np.log10(ground_truth[2])
 
-nvs = [5, 25, 50]
+prior_lower = [36, 0, np.log10(3e2), 18, -5, 8, 0, -4]
+prior_upper = [41, 1, np.log10(5e5), 23, 0, 15, 2, 0]
+
+nvs = [5, 25]
 FIXED_NOISE = True
 PLOT_POSTERIOR = False
-PLOT_BIAS = True
-CALCULATE_KL = False
+PLOT_BIAS = False
+CALCULATE_KL = True
 
 if FIXED_NOISE:
     names = ['cX', 'fesc', 'Tmin', 'logN', 'fstar', 'Mp', 'gamma_low', 'gamma_high']
@@ -82,11 +85,14 @@ if CALCULATE_KL:
     from margarine.marginal_stats import calculate
     import tensorflow as tf
 
+    fig, axes = plt.subplots(1, 2, figsize=(6, 4))
     for j, nv in enumerate(nvs):
+        z, dT_obs = np.loadtxt('ares_fiducial_model_noise_%d.txt' % nv, unpack=True)
+
         ares = read_chains(f'ares_fiducial_model_noise_{nv}_ARES_True_FIXED_NOISE_True/test')
         emu = read_chains(f'ares_fiducial_model_noise_{nv}_ARES_False_FIXED_NOISE_True/test')
-        #ares= chains_ares.compress(5000)
-        #emu = chains_emu.compress(5000)
+        ares = ares.compress(15000)
+        emu = emu.compress(15000)
 
         ares['cX'] = np.log10(ares['cX'])
         ares['Mp'] = np.log10(ares['Mp'])
@@ -99,9 +105,16 @@ if CALCULATE_KL:
 
         ares_samples = ares[names].values.astype(np.float32)
         emu_samples = emu[names].values.astype(np.float32)
+        print(ares_samples.shape, emu_samples.shape)
 
-        maf_ares = MAF(ares_samples)
-        maf_emu = MAF(emu_samples)
+        nsamples = int(np.min([ares_samples.shape[0], emu_samples.shape[0]]))
+        ares_samples = ares_samples[:nsamples]
+        emu_samples = emu_samples[:nsamples]
+
+        maf_ares = MAF(ares_samples, weights=ares.get_weights()[:nsamples],
+                       theta_min=prior_lower, theta_max=prior_upper)
+        maf_emu = MAF(emu_samples, weights=emu.get_weights()[:nsamples],
+                      theta_min=prior_lower, theta_max=prior_upper)
 
         try:
             maf_ares = MAF.load(f'{nv}_maf_ARES_True_FIXED_NOISE_True.pkl')
@@ -112,7 +125,39 @@ if CALCULATE_KL:
             maf_ares.save(f'{nv}_maf_ARES_True_FIXED_NOISE_True.pkl')
             maf_emu.save(f'{nv}_maf_ARES_False_FIXED_NOISE_True.pkl')
 
-        stats = calculate(maf_emu, prior_de=maf_ares).statistics()
+        stats = calculate(maf_ares, prior_de=maf_emu, 
+                          samples=maf_ares.sample(nsamples)).statistics()
         print(f'{nv} :', stats['KL Divergence'], ' - ',
               stats['KL Divergence'] - stats['KL Lower Bound'], ' + ',
               stats['KL Upper Bound'] - stats['KL Divergence'])
+
+        nd = len(dT_obs)
+        epsilon = np.linspace(0., 0.25, 100)
+        min_error = 0.11/nv
+        emulator_error = 0.82/nv
+        max_error = 2.56/nv
+        limit = 0.5*nd*(epsilon)**2
+        print(np.array([stats['KL Divergence'] - stats['KL Lower Bound'],
+                        stats['KL Upper Bound'] - stats['KL Divergence']]))
+        axes[0].errorbar(emulator_error, stats['KL Divergence'],
+                      yerr=np.array([stats['KL Divergence'] - stats['KL Lower Bound'],
+                        stats['KL Upper Bound'] - stats['KL Divergence']]).reshape(2, 1),
+                        #xerr=[[emulator_error - min_error], [max_error - emulator_error]],
+                      fmt='x', label=f'Noise = {nv}')
+        sigmas = np.linspace(0, 60, 100)
+        axes[1].errorbar(nv, emulator_error*nv, 
+                         yerr=[[emulator_error*nv - min_error*nv], [max_error*nv - emulator_error*nv]],
+                         fmt='x', label=r'$\sigma = $ %.2f mK' % nv)
+    axes[1].plot(sigmas, sigmas*np.sqrt(2/nd), c='k')
+    axes[1].set_xlabel(r'$\sigma$')
+    axes[1].set_ylabel(r'$\mathrm{RMSE}$')
+    axes[0].loglog()
+    axes[0].plot(epsilon, limit, c='k')
+    axes[0].set_xlabel(r'$\mathrm{RMSE}/\sigma$')
+    axes[0].set_ylabel(r'$D_{KL}$')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('kl_divergence.png', dpi=300)
+    plt.close()
+    #plt.show()
+
